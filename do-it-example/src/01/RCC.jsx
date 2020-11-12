94d1aaebb86bf76474be1ef19202db854f8e0bd8
Miri Ryu <natsu12@naver.com>
2020-11-13 01:08:21 +0900

    [ADD]프로젝트 폴더 일부_20201113

---
+++ do-it-example/src/01/RCC.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class RCC extends Component {
+  render() {
+    var text = '따옴표';
+    return <div name="name">{text}</div>;
+  }
+}
+
+export default RCC;
+
+/*
+** Reactjs code snippet 자주 사용 단축 키워드 **
+RCC : 기본 리액트 컴포넌트 코드 생성
+RCCP: 리액트 컴포넌트를 프로퍼티 타입과 함께 생성
+RCFC: 리액트 컴포넌트를 생명주기 함수와 함께 생성
+RPC: 리액트 퓨어 컴포넌트를 생성
+RSC: 함수형 컴포넌트를 생성
+RSCP: 함수형 컴포넌트를 프로퍼티 타입과 함께 생성
+*/
